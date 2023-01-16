ad71b5cbfb202d9715d172a33f4c789d9ee5a09a
RayaneDosAnjos <raydosanjos98@gmail.com>
2023-01-16 00:33:42 -0300

    Header criado

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Contato</li>
+          <li>Sobre nós</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default Header
