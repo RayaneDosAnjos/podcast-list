37d58e16a982a7b563ddff2af42c252bbf1070f3
RayaneDosAnjos <raydosanjos98@gmail.com>
2023-01-19 01:35:57 -0300

    slider adicionado

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,20 +1,29 @@
 import style from "./Header.module.css"
 const Header = () => {
   return (
-    <header className={style.header}>
+    <header className={style.header} >
       
       <nav className={style.nav}>
         <div className={style.navItem}>
           <div>Menu</div>
           <div>Logo</div>
-          <div>Filmes e séries a vontade</div>
+          <div className={style.line}></div>
+          <div>Filmes e Séries à Vontade</div>
         </div>
         <div className={style.navItem}>
           <div>Lupa</div>
-          <button type="submit">Entrar</button>
+          <button type="submit" className={style.btnLogin}>ENTRAR</button>
         </div>
       </nav>
     </header>
   );
 };
+
+export function Test (){
+  
+  return(
+      <div>oi,teste</div>
+  )
+
+}
 export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,28 @@
 import Header from "../components/Header"
-function Home(){
-  return (<div><Header/></div>)
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react"
+import 'swiper/css';
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
+
+
+function Home() {
+  return (
+    <div style={{ height: '428px' }} >
+      <Header />
+
+      <Swiper spaceBetween={50} pagination={true} modules={[Pagination]} className="mySwiper">
+        <SwiperSlide>
+
+          <img src="https://ottvsimg.ottvs.com.br/res/banner/641cf78f-5da5-42a5-915c-dfd033a47391.jpg"></img>
+        </SwiperSlide>
+        <SwiperSlide>oi</SwiperSlide>
+        <SwiperSlide>oi aDAD</SwiperSlide>
+        <SwiperSlide>oi adad</SwiperSlide>
+        <SwiperSlide>o fsdfsdf  i</SwiperSlide>
+        <SwiperSlide>oisdfsdf</SwiperSlide>
+      </Swiper>
+    </div>)
 }
 
 export default Home
